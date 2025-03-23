39c5ee950d4dff4ebaf03f4a6f91785b1de67a7c
Seonyoung <joysyl0210@gmail.com>
2025-03-23 18:20:14 -0400

    Added features to display, flip, and add cards

--- script.js
+++ script.js
@@ -9,13 +9,59 @@ const flashcards = [
 // These two variables will come in handy
 let currentIndex = 0;
 let showingTerm = true;
+let cardDiv = document.getElementById('flashcard')
+let text = document.getElementById("card-content")
 
 // Start with this function to simply display the card
 function displayCard() {
+    let currCard = flashcards[currentIndex]
+    let front = currCard.term
 
+    text.innerText = front
+    cardDiv.appendChild(text)
+    showingTerm = true
 }
 
 // The rest of the code you will write is apart of event listeners
+function flipCard() {
+    
+    if (showingTerm === true) {
+        showingTerm = false;
+        text.innerText = flashcards[currentIndex].definition
+        
+    } else {
+        showingTerm = true;
+        text.innerText = flashcards[currentIndex].term
+    }
+
+}
+
+function next() {
+    if (++currentIndex >= flashcards.length) {
+        currentIndex = 0
+    }
+    displayCard();
+}
+
+function prev() {
+    if (--currentIndex < 0) {
+        currentIndex = flashcards.length - 1
+    }
+    displayCard();
+}
+
+function addCard() {
+    flashcards.push({
+        term: document.getElementById("new-term").value,
+        definition: document.getElementById("new-definition").value
+    });
+    document.getElementById("new-term").value = ""
+    document.getElementById("new-definition").value = ""
+}
 
 // This line will display the card when the page is refreshed
 window.onload = displayCard;
+cardDiv.addEventListener('click', ()=> flipCard())
+document.getElementById("next-btn").addEventListener('click', ()=> next())
+document.getElementById("prev-btn").addEventListener('click', ()=> prev())
+document.getElementById("add-card-btn").addEventListener('click', ()=> addCard())
